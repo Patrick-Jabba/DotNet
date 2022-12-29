79a3e84b962599226f4f2136ad01caa02431294f
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-12-29 12:10:16 -0300

    FilmesApi Model Post [Validacao e Retorno] Get [Paginacao e Tratamento]

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O título do filme é obrigatório.")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O gênero do filme é obrigatório.")]
+    [MaxLength(50, ErrorMessage = "Tamanho de gênero excedido.")]
+    public string Genero { get; set; }
+
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos.")]
+    public int Duracao { get; set; }
+}
